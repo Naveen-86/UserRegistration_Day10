b7ae78c78ed86236d9c9028f11841cde02cda530
Naveen <naveentruly86@gmail.com>
2021-06-03 11:33:15 +0530

    Added: Test Cases for Email Validation

--- src/test/java/com/uservalidation/ValidatorTest.java
+++ src/test/java/com/uservalidation/ValidatorTest.java
@@ -1,28 +1,13 @@
 package com.uservalidation;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ValidatorTest {
     @Test
-    void givenLastName_itIsValid_returnTrue() {
+    void givenEmail_itIsValid_returnTrue() {
         Validator validator = new Validator();
-        boolean status = validator.validateLastName("Labz");
-        assertEquals(true,status);
-    }
-
-    @Test
-    void givenFirstName_whenShort_returnfalse() {
-        Validator validator = new Validator();
-        boolean status = validator.validateLastName("Br");
-        assertEquals(false,status);
-    }
-
-    @Test
-    void givenFirstName_whenInValid_returnfalse() {
-        Validator validator = new Validator();
-        boolean status = validator.validateLastName("bridgelabz");
-        assertEquals(false,status);
+        boolean status = validator.validateEmail("abc.xyz@bl.co.in");
+        Assertions.assertEquals(true,status);
     }
 }
