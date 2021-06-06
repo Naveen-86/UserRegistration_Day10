7a1c9eb8f0f949c9ea00c6e02998715c8a2f41ef
Naveen <naveentruly86@gmail.com>
2021-06-06 08:57:13 +0530

    Added: Parameterised Test Cases For Multiple Emails

--- src/test/java/com/uservalidation/ValidatorTest.java
+++ src/test/java/com/uservalidation/ValidatorTest.java
@@ -1,41 +1,36 @@
 package com.uservalidation;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+import org.junit.runner.RunWith;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
+@RunWith(Parameterized.class)
 public class ValidatorTest {
-    @Test
-    void givenFirstName_itIsValid_returnTrue() {
-        Validator validator = new Validator();
-        boolean status = validator.validateFirstName("Bridgelabz");
-        Assertions.assertEquals(true,status);
+    public String email;
+    public boolean status;
+    Validator validator = new Validator();
+
+    public ValidatorTest(String email, boolean status) {
+        this.email = email;
+        this.status = status;
+    }
+
+    @Parameterized.Parameters
+    public static Collection input() {
+        return Arrays.asList(new Object[][]{{"abc@yahoo.com",true},{"abcyahoo.com",false},{"abc-100@yahoo.com",true},{"abc.100@yahoo.com",true},{"abc111@abc.com",true},{"abc-100@abc.net",true},{"abc.100@abc.com.au",true},{"abc@1.com",true},{"abc@gmail.com.com",true},{"abc+100@gmail.com",true},{"abc",false},{"abc@.com.my",false},{"abc123@gmail.a",false},{"abc123@.com",false},{"abc123@.com.com",false},{".abc@abc.com",false},{"abc()*@gmail.com",false},{"abc@%*.com",false},{"abc..2002@gmail.com",false},{"abc.@gmail.com",false},{"abc@abc@gmail.com",false}});
     }
 
     @Test
-    void givenLastName_itIsValid_returnTrue() {
-        Validator validator = new Validator();
-        boolean status = validator.validateLastName("Bridgelabz");
-        Assertions.assertEquals(true,status);
-    }
-
-    @Test
-    void givenEmail_itIsValid_returnTrue() {
-        Validator validator = new Validator();
-        boolean status = validator.validateEmail("abc.xyz@bl.co.in");
-        Assertions.assertEquals(true,status);
-    }
-
-    @Test
-    void givenMobileNumber_itIsValid_returnTrue() {
-        Validator validator = new Validator();
-        boolean status = validator.validateMobileNumber("+91 9919819801");
-        Assertions.assertEquals(true,status);
-    }
-
-    @Test
-    void givenPasswordRule4_itIsValid_returnTrue() {
-        Validator validator = new Validator();
-        boolean status = validator.validatePassword("Nicena@1");
-        Assertions.assertEquals(true,status);
+    public void emailChecker() {
+        try {
+            System.out.println(this.email);
+            boolean result = validator.validateEmail(email);
+            Assert.assertEquals(this.status, result);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
